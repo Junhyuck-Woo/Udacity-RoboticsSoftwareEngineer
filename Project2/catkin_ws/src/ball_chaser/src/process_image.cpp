f33130c7fee2bf72be895424dd049f4dd1d17251
JunhyuckWoo <junhyuck@usc.edu>
2020-10-27 01:58:28 +0900

    Add the Error control part on drive_robot function

--- Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -12,6 +12,10 @@ void drive_robot(float lin_x, float ang_z)
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
+
+    if(!client.call(srv)) {
+        ROS_ERROR("Fail to call service drive_bot");
+    }
 }
 
 // This callback function continuously executes and reads the image data
